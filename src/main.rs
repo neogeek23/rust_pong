991a1c3571c706ab4830fae4fabec661eae44cff
brad <brad.stein@gmail.com>
2019-04-21 17:29:37 -0500

    first stage of pong tutorial

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate amethyst;
+
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline,
+                         RenderBundle, Stage, VirtualKeyCode};
+use amethyst::utils::application_root_dir;
+
+pub struct Pong;
+
+impl SimpleState for Pong {
+}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+    let config = DisplayConfig::load(&path);
+    let pipe = Pipeline::build()
+    .with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+    );
+    
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+      RenderBundle::new(pipe, Some(config))
+        .with_sprite_sheet_processor()
+    )?;
+
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    Ok(())
+}
