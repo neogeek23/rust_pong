8d74a3ca408a4960ede11ea03508833002b0e005
brad <brad.stein@gmail.com>
2019-04-22 23:31:35 -0500

    ball movement and reaction to paddle added

--- src/pong.rs
+++ src/pong.rs
@@ -11,6 +11,9 @@ use amethyst::renderer::{
 pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
+pub const BALL_VELOCITY_X: f32 = 65.0;
+pub const BALL_VELOCITY_Y: f32 = 40.0;
+pub const BALL_RADIUS: f32 = 2.0;
 
 const PADDLE_WIDTH: f32 = 4.0;
 
@@ -22,6 +25,10 @@ impl SimpleState for Pong {
         let sprite_sheet_handle = load_sprite_sheet(world);
 
         //world.register::<Paddle>();
+        world.register::<Ball>();
+
+
+        initialise_ball(world, sprite_sheet_handle.clone());
         initialise_paddles(world, sprite_sheet_handle);
         initialise_camera(world);
     }
@@ -50,6 +57,15 @@ impl Paddle {
 }
 
 impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Ball {
     type Storage = DenseVecStorage<Self>;
 }
 
@@ -98,6 +114,29 @@ fn initialise_paddles(world: &mut World, sprite_sheet: SpriteSheetHandle){
         .with(Flipped::Horizontal)
         .with(Paddle::new(Side::Right))
         .with(right_transform)
+        .build();
+}
+
+/// Initialises one ball in the middle-ish of the arena.
+fn initialise_ball(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
+    // Create the translation.
+    let mut local_transform = Transform::default();
+    local_transform.set_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite for the ball
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 1, // ball is the second sprite on the sprite sheet
+    };
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
         .build();
 }
 
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,3 +1,7 @@
 mod paddle;
+mod move_balls;
+mod bounce;
 
 pub use self::paddle::PaddleSystem;
+pub use self::bounce::BounceSystem;
+pub use self::move_balls::MoveBallsSystem;
